e9546a0cdd092e7c462ad53ec69bafd0c588d476
Gabriel Mors <gabrielmors210@gmail.com>
2023-06-17 06:45:39 -0400

    [FIX] created elements screen

--- AppCoreData/View/Screen.swift
+++ AppCoreData/View/Screen.swift
@@ -9,4 +9,54 @@ import UIKit
 
 class Screen: UIView {
     
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.showsVerticalScrollIndicator = false
+//        tableView.register(<#T##UINib?#>, forCellReuseIdentifier: <#T##String#>)
+        return tableView
+    }()
+    
+    lazy var nameTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.font = UIFont.systemFont(ofSize: 18)
+        textField.placeholder = "type your name"
+        textField.autocorrectionType = .no
+        textField.spellCheckingType = .no
+        textField.keyboardType = .default
+        return textField
+    }()
+    
+    lazy var ageTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.font = UIFont.systemFont(ofSize: 18)
+        textField.placeholder = "type your age"
+        textField.autocorrectionType = .no
+        textField.spellCheckingType = .no
+        textField.keyboardType = .default
+        return textField
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubViews()
+        configConstraints()
+    }
+    
+    private func addSubViews() {
+        addSubview(tableView)
+        addSubview(nameTextField)
+        addSubview(ageTextField)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configConstraints() {
+        
+    }
+    
 }
